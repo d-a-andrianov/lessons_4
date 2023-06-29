2a33c8e31b106907c81fccc4e1fd33d4437044ee
Дмитрий Андрианов <d.a.andrianow@gmail.com>
2023-06-29 09:43:18 +0300

    дз 4_е

--- src/js/einstein.js
+++ src/js/einstein.js
@@ -10,3 +10,11 @@
   console.log (`Энергия моего тела равна ${energy} Дж`);
 }
 
+// {
+//   const myWeight = '92';
+//   const speedLight = '3e8';
+
+//   const energy = (myWeight * speedLight);
+//   console.log (`Энергия моего тела равна ${energy} Дж`);
+// }
+
