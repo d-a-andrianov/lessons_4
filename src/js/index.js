01af0a9fb9666e34483eea2376d925f064357eec
Дмитрий Андрианов <d.a.andrianow@gmail.com>
2023-06-30 16:45:14 +0300

    дз 4.3

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 // 'use strict';
 
-// // ДЗ 4.1_Задача задать переменные, получить стоимость товаров
+// ДЗ 4.1_Задача задать переменные, получить стоимость товаров
 
 // const productName = 'Навигационная система Soundmax';
 // const productUnits = '5';
@@ -13,35 +13,35 @@
 // console.log(productName);
 // console.log(productResult + productCurrency);
 
-// // ДЗ 4.2_1 Задача_Запросите у пользователя "Наименование товара" с помощью функции prompt и запишите в константу. В консоль выведите строку со значением количество товара, наименование и общую сумму 
+// ДЗ 4.2_1 Задача_Запросите у пользователя "Наименование товара" с помощью функции prompt и запишите в константу. В консоль выведите строку со значением количество товара, наименование и общую сумму 
 
-// {
-//   const productName = prompt('Укажите наименование товара');
-//   console.log(typeof productName);
+{
+  const productName = prompt('Укажите наименование товара');
+  console.log(typeof productName);
 
-//   const productUnits = +prompt('Укажите количество товара');
-//   console.log(typeof productUnits);
+  const productUnits = +prompt('Укажите количество товара');
+  console.log(typeof productUnits);
 
-//   const productCategory = prompt('Укажите категорию товара');
-//   console.log(typeof productName);
+  const productCategory = prompt('Укажите категорию товара');
+  console.log(typeof productName);
 
-//   const productPrice = +prompt('Укажите цену товара');
-//   console.log(typeof productUnits);
+  const productPrice = +prompt('Укажите цену товара');
+  console.log(typeof productUnits);
 
-//   const productCurrency = 'деревянных';
+  const productCurrency = 'деревянных';
 
-//   const productResult = (productUnits * productPrice);
+  const productResult = (productUnits * productPrice);
 
-//   console.log(`На складе ${productUnits} единицы товара "${productName}" на сумму ${productResult} ${productCurrency}`);
+  console.log(`На складе ${productUnits} единицы товара "${productName}" на сумму ${productResult} ${productCurrency}`);
 
-//   // ДЗ 4.3_1 Задача_проверка что пользователь ввёл число, выдать ошибку если не число
+  // ДЗ 4.3_1 Задача_проверка что пользователь ввёл число, выдать ошибку если не число
 
-//   if (isNaN(productUnits || productPrice)) {
-//     console.log(`На складе ${productUnits} единицы товара "${productName}" на сумму ${productResult} ${productCurrency}`);
+  if (isNaN(productUnits || productPrice)) {
+    console.log(`На складе ${productUnits} единицы товара "${productName}" на сумму ${productResult} ${productCurrency}`);
 
-//     if (isNaN(productUnits || productPrice)) {
-//       alert('Вы ввели некорректные данные');
-//     }
-//   }
-// }
+    if (isNaN(productUnits || productPrice)) {
+      alert('Вы ввели некорректные данные');
+    }
+  }
+}
 
