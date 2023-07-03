fd3aa12af9609b906f7007340fb857e86203b842
Дмитрий Андрианов <d.a.andrianow@gmail.com>
2023-07-03 16:59:32 +0300

    правки по дз 4.3

--- src/js/lesson-03/tasksHard_2.js
+++ src/js/lesson-03/tasksHard_2.js
@@ -4,15 +4,15 @@
   const salary = +prompt('Укажите размер заработной платы');
   let tax = 0;
 
-  if (salary <= 15000) {
+  if (salary < 15000) {
     tax += (salary * 0.13);
     console.log(`Налог с заработной платы: ${tax}`);
   } if
-    (salary >= 15000 && salary < 50000) {
+    (salary >= 15000 && salary <= 50000) {
     tax += ((salary - 15000) * 0.2) + (15000 * 0.13);
     console.log(`Налог с заработной платы: ${tax}`);
   } if
-    (salary >= 50000) {
+    (salary > 50000) {
     tax += (((50000 - 15000) * 0.3) + ((salary - 15000) * 0.2) + (15000 * 0.13));
     console.log(`Налог с заработной платы: ${tax}`);
   }
