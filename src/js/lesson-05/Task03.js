fa8e304c7af56b7a3a4821261fab15248c10a6ef
Дмитрий Андрианов <d.a.andrianow@gmail.com>
2023-07-06 15:35:24 +0300

    ДЗ 4.5

---
+++ src/js/lesson-05/Task03.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Задача 4.5.3
+// Напишите функцию, которая принимает строку и возвращает обратную строку
+// Пример: "Привет мир" => "рим тевирП"
+
+
+function reverseStr(str) {
+
+  var newStr = '', i;
+  for (i = str.length - 1; i >= 0; i--) {
+    newStr += str.charAt(i);
+  }
+  return newStr;
+  b = str.length;
+}
+console.log (reverseStr('Привет мир!'));
