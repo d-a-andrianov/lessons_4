4a5248063076265064994dbcabb01b6db3449a6e
Дмитрий Андрианов <d.a.andrianow@gmail.com>
2023-07-06 13:52:01 +0300

    ДЗ 4.5

--- src/js/lesson-05/Task01.js
+++ src/js/lesson-05/Task01.js
@@ -1,6 +1,18 @@
+// Задача 4.5.1
+// Выведите в консоль таблицу степеней от 1 до 10 по аналогии как в уроке выводится таблица умножения
+
 'use strict'
 
-for (let i = 0; i < 10; i += 1) {
-    if (i % 3) continue;
-    console.log(i)
+  for (let i = 1; i < 11; i++ ) {
+    console.log('----------');
+    for (let y = 1; y < 11; y++) {
+      console.log(`${i}'${y} = ${i ** y} `);
+  }
 }
+
+
+
+// for (let i = 0; i < 10; i += 1) {
+//     if (i % 3) continue;
+//     console.log(i)
+// }
