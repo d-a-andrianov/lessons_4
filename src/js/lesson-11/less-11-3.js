512cc40b96d48db388b55ca238fd98ba574be31b
Дмитрий Андрианов <d.a.andrianow@gmail.com>
2023-09-11 17:55:45 +0300

    ДЗ 11.3.3

--- src/js/lesson-11/less-11-3.js
+++ src/js/lesson-11/less-11-3.js
@@ -1,52 +1,61 @@
+/* eslint-disable no-setter-return */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 'use strict';
 
-const figure = {
-  width: 5,
-  height: 5,
-}
+// const figure = {
+//   width: 5,
+//   height: 5,
+// }
 
-Object.defineProperty(figure, 'width', 'height', {
+// Object.defineProperty(figure, 'width', 'height', {
 
-  set(width) {
-    return `${this.width}`
-  },
+//   set(width) {
+//     return `${this.width}`
+//   },
 
-  set(height) {
-    return `${this.height}`
-  },
+//   set(height) {
+//     return `${this.height}`
+//   },
+
+//   get perimeter() {
+//     perimeter = (this.width + this.height)*2;
+//   },
+
+//   get square() {
+//     square = (this.width * this.height);
+//   },
+// });
+
+// console.log(square);
+
+
+const rectangle = {
+  _width: 5,
+  _height: 5,
 
   get perimeter() {
-    perimeter = (this.width + this.height)*2;
+    return (this._width + this._height) * 2 + ' sm*2';
   },
 
   get square() {
-    square = (this.width * this.height);
+    return this._width * this._height + ' sm*2';
   },
-});
 
-console.log(square);
+  set width(newWidth) {
+    if (typeof newWidth === 'number') {
+      this._width = newWidth;
+    }
+  },
 
+  set height(newHeight) {
+    if (typeof newHeight === 'number') {
+      this._height = newHeight;
+    }
+  },
+};
 
-// const rectangle = {
-//   _width: 5,
-//   _height: 5,
-
-//   get perimeter() {
-//     return .....
-//   },
-//   get square() {
-//     return .....
-//   },
-
-//   set width(newWidth) {
-//     if (typeof newWidth === 'number') {
-// ....
-//     }
-//   },
-
-//   set height(newHeight) {
-//     if (typeof newHeight === 'number') {
-// ....
-//     }
-//   },
-// };
+rectangle.width = 100;
+rectangle.height = '8';
+console.log(rectangle.perimeter);
+console.log(rectangle.square);
